55395f95124e4cdc21705998757fb1a731f2d799
Brian Gudely <bcgudely@gmail.com>
2016-07-29 16:02:17 -0700

    finishing up specing out userRepository

--- db/userRepository.js
+++ db/userRepository.js
@@ -26,3 +26,30 @@ exports.CreateUser = function (req, callback) {
     callback(results);
   });
 };
+
+exports.UpdateUser = function (req, callback) {
+  UserSchema.findOne({ _id: req._id }, function (err, user) {
+    if (err) throw err;
+    if (!user) { callback(user); } else {
+      user.username = req.username || user.username;
+      user.password = req.password || user.password;
+
+      user.save(function (err, results) {
+        if (err) throw err;
+        callback(results);
+      });
+    }
+  });
+};
+
+exports.DeleteUser = function (id, callback) {
+  UserSchema.findOne({ _id: id }, function (err, user) {
+    if (err) throw err;
+    if (!user) { callback(user); } else {
+      user.remove(function (err, results) {
+        if (err) throw err;
+        callback(results);
+      });
+    }
+  });
+};
